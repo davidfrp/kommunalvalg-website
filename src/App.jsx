51269c038ecc46e19bfee37c56a72b26a0b2db51
davidfrp <davidfrp@outlook.dk>
2021-12-20 21:40:46 +0100

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import partyService from './services/partyService';
+import { useState, useEffect } from 'react';
+import PartyList from './components/PartyList';
+
 function App() {
+    const [parties, setParties] = useState([]);
+    
+    useEffect(() => {
+        async function fetchData() {
+            setParties(await partyService.findAll());
+        }
+        fetchData();
+    }, []);
+
     return (
         <main>
             <h1 className="text-3xl font-bold underline">
                 Hello world!
             </h1>
+            <PartyList parties={parties} />
         </main>
     );
 }
